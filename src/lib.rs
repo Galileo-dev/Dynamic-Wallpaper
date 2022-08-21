ee700bdc03fce1ccb555f969266e2a84798cb670
Galileo <Galileo-dev@users.noreply.github.com>
2022-08-21 14:04:05 +0100

    added

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,21 @@ pub mod list;
 pub mod loc;
 pub mod sun;
 pub mod xml;
+
+#[cfg(test)]
+mod tests {
+  use crate::heic::decode_heic;
+  use color_eyre::eyre::Result;
+  use std::{fs, path::PathBuf};
+
+  #[test]
+  fn decode_heic_image() -> Result<()> {
+    let image_path = PathBuf::from("./test/image.heic");
+    assert!(image_path.exists());
+    let export_path = PathBuf::from("./test/export/test01");
+    fs::create_dir_all(&export_path)?;
+    assert!(export_path.exists());
+    decode_heic(image_path, export_path)?;
+    Ok(())
+  }
+}
